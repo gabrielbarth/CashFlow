f4c78ebaaee1c5795a31428fbe0b1e721371ab33
gabrielbarth <gabriel.silverio@stone.com.br>
2024-09-29 23:42:27 -0400

    chore: add compose and material3 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,10 @@ android {
         jvmTarget = "1.8"
     }
     buildFeatures {
-        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
     }
 }
 
@@ -48,4 +51,13 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation("androidx.compose.ui:ui:1.5.1")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.navigation:navigation-compose:2.7.4")
+    debugImplementation(libs.androidx.ui.tooling)
+
 }
